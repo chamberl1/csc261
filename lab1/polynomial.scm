67ce6ccd523a4d65e160147664ba3ac8abe81a56
Martin Chamberlin <chamberl1@cs.grinnell.edu>
2018-01-29 20:31:18 -0600

    halfway

--- lab1/polynomial.scm
+++ lab1/polynomial.scm
@@ -19,19 +19,57 @@
 ;;   proc, a binary procedure
 ;;   right, a value
 ;; Purpose:
-;;
+;;   to create a function that runs proc with fixed right value
+;; Preconditions:
+;;   proc must be a function that takes two parameters
+;;   right must be a valid value for proc's right parameter
+;; Postconditions:
+;;   returns a function that runs proc with the given right value
 (define right-section
   (lambda (proc right)
     (lambda (left)
       (proc left right))))
 
+
+;; Purpose:
+;;   computes the dot product of two lists
+;; Preconditions:
+;;   v1 and v2 must be integer lists of equal length
+;; Postconditions:
+;;   returns an integer that is the dot product
 (define dot-product
   (lambda (v1 v2)
-    ()))   ;;I DONT REMEMBER SCHEME
+    (apply + (map * v1 v2))))
 
+
+;; Purpose:
+;;   to create a list of numbers up to a point
+;; Preconditions:
+;;   num must be an integer greater than 0
+;; Postconditions:
+;;   returns a list of integers from 0 up to num-1, ascending
 (define iota
   (lambda (num)
-    ()))
+    (let f((lst '()) (i (- num 1)))
+      (if (< i 0) lst
+         (f (cons i lst) (- i 1) )))))
+
+;; Purpose:
+;;   to create a function that runs a paraticular polynomial function
+;; Preconditions:
+;;   c and n must be reals
+;; Postconditions:
+;;   returns the function f(x)=c*x^n
+(define polynomial-term
+  (lambda (c n)
+    (lambda (x)
+      (* c (expt x n)))))
+
+(define polynomial-derivative-coeffs
+  (lambda (lst)
+    (
 
 
+(define lst1 '(1 2 3 4))
+(define lst2 '(4 5 6 7))
      
